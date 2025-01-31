7bf22f3607d5d14e01607156557211203a2921c3
shiiraz <this.shiraz@gmail.com>
2025-01-31 21:25:57 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // Important for Cloudflare Pages
+  reactStrictMode: true,
 };
 
 export default nextConfig;
