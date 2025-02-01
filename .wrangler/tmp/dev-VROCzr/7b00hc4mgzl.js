7f9e7091bb787149a9aee91c3b003445a643847e
shiiraz <this.shiraz@gmail.com>
2025-02-01 20:09:57 +0500

    more updates

--- .wrangler/tmp/dev-VROCzr/7b00hc4mgzl.js
+++ .wrangler/tmp/dev-VROCzr/7b00hc4mgzl.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-YW18kI/checked-fetch.js
+// .wrangler/tmp/bundle-W8JJLS/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -942,7 +942,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-YW18kI/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-W8JJLS/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -974,7 +974,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-YW18kI/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-W8JJLS/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
